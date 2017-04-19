57e1ca9aff60754455fc2dc76493405807142a04
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-19 01:08:05 -0500

    feat: work order heading

---
+++ tests/unit/ticket/detail/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:ticket/detail', 'Unit | Route | ticket/detail', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});

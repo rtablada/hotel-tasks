cb77a78dc89b5e0d61dddaeae856f41261a0884a
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-19 08:56:10 -0500

    fix: detail route needs service scroller for test

--- tests/unit/ticket/detail/route-test.js
+++ tests/unit/ticket/detail/route-test.js
@@ -2,7 +2,7 @@ import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('route:ticket/detail', 'Unit | Route | ticket/detail', {
   // Specify the other units that are required for this test.
-  // needs: ['controller:foo']
+  needs: ['service:scroller'],
 });
 
 test('it exists', function (assert) {

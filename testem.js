c71bd632a0e55a14e9df4ef3306588a7dff7791d
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-19 08:37:33 -0500

    fix: use phantom for CI tests

--- testem.js
+++ testem.js
@@ -3,7 +3,7 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Firefox',
+    'PhantomJS',
   ],
   launch_in_dev: [
     'Firefox',

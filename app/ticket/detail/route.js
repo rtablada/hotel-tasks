35a414017503b23a50cab2f9db8098d208ded11c
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-19 08:44:19 -0500

    feat: scroll to detail on mobile

--- app/ticket/detail/route.js
+++ app/ticket/detail/route.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  scroller: Ember.inject.service(),
+
+  afterModel() {
+    this.get('scroller').scrollVertical('.master-detail__panel-fill');
+  },
 });

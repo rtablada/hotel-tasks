63c2f90555f720be34d2f885d699521c2322e2e7
Ryan Tablada <ryan.tablada@gmail.com>
2017-04-18 23:11:28 -0500

    style: ticket item

--- src/router.js
+++ src/router.js
@@ -3,10 +3,13 @@ import config from '../config/environment';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
 });
 
-Router.map(function() {
+Router.map(function () {
+  this.route('ticket', { path: '/' }, function () {
+    this.route('detail', { path: '/:ticket_id' });
+  });
 });
 
 export default Router;
